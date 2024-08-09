3acd2c6976e71f7f34ae53933e1fdd32c2e3fb0f
kanghowoo <kanghowoo@github.com>
2024-08-09 13:32:50 +0900

    design: overall pick-ban template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '300px': '300px',
+        '1/10': '10.0%',
+        '9/10': '90.0%',
+      },
+      minWidth: {
+        '18': '72px',
+        '1280px': '1280px',
+      },
+      height: {
+        '108px': '108px',
+        '540px': '540px',
+      },
+    },
   },
   plugins: [],
 };
