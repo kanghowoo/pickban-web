3e60c0730eec0deda7f467de0e35ee5af8b7c048
kanghowoo <tiger452003@gmail.com>
2024-08-13 23:59:29 +0900

    add: pick-ban component (parent component)

--- src/app/pick-ban/pick-ban.component.ts
+++ src/app/pick-ban/pick-ban.component.ts
@@ -1,39 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Player } from '../player.model';
+import { TeamPlayerComponent } from "../team-player/team-player.component";
+import { ChampionListComponent } from "../champion-list/champion-list.component";
+import { PickBanOrderComponent } from "../pick-ban-order/pick-ban-order.component";
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-pick-ban',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TeamPlayerComponent, ChampionListComponent, PickBanOrderComponent],
   templateUrl: './pick-ban.component.html',
   styleUrls: ['./pick-ban.component.css']
 })
-export class PickBanComponent implements OnInit {
-  champions: any[] = [];
-  selectedChampion: any = null;
-  blueTeam = [
-    { name: 'Rengar', image: 'assets/rengar.png' },
-    { name: 'Renata', image: 'assets/renata.png' },
-    { name: 'Neeko', image: 'assets/neeko.png' },
-    { name: 'player4', image: 'assets/default.png' },
-    { name: 'player5', image: 'assets/default.png' },
-  ];
+export class PickBanComponent {
 
-  redTeam = [
-    { name: 'Renekton', image: 'assets/renekton.png' },
-    { name: 'Leona', image: 'assets/leona.png' },
-    { name: 'Rumble', image: 'assets/rumble.png' },
-    { name: 'player9', image: 'assets/default.png' },
-    { name: 'player10', image: 'assets/default.png' },
-  ];
+  blueTeam = this.dataService.getBlueTeamPlayers();
+  redTeam = this.dataService.getRedTeamPlayers();
+
   constructor(private dataService: DataService) {}
 
-  ngOnInit() {
-    this.champions = this.dataService.getChampions();
-  }
-
-  selectChampion(champion: any) {
-    this.selectedChampion = champion;
-  }
 }
