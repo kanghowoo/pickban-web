c5d64e50788118d6986509344e4327f6f4b9b601
kanghowoo <tiger452003@gmail.com>
2024-08-23 02:48:29 +0900

    design: update css

--- src/app/pick-ban/pick-ban.component.ts
+++ src/app/pick-ban/pick-ban.component.ts
@@ -5,11 +5,13 @@ import { ChampionListComponent } from "../champion-list/champion-list.component"
 import { PickBanOrderComponent } from "../pick-ban-order/pick-ban-order.component";
 import { DataService } from '../data.service';
 import { TeamBanComponent } from "../team-ban/team-ban.component";
+import { MatchInfoComponent } from "../match-info/match-info.component";
 
 @Component({
   selector: 'app-pick-ban',
   standalone: true,
-  imports: [CommonModule, TeamPlayerComponent, ChampionListComponent, PickBanOrderComponent, TeamBanComponent],
+  imports: [CommonModule, TeamPlayerComponent, ChampionListComponent, 
+    PickBanOrderComponent, TeamBanComponent, MatchInfoComponent],
   templateUrl: './pick-ban.component.html',
   styleUrls: ['./pick-ban.component.css']
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,14 @@ module.exports = {
         '18': '72px',
       },
       maxWidth: {
-        'champion-list': '33.75rem'
+        'champion-list': '33.75rem',
+        'pick-ban-result': '76rem',
       },
       minWidth: {
         '18': '72px',
         '480px': '480px',
         'champioin-list': '33.75rem',
-        '1280px': '1280px',
+        'pick-ban-result': '76.25rem',
       },
       height: {
         '108px': '108px',
